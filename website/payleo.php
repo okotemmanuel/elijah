25cb22baa46c3c1f790c5ca90b1c11eaf659af32
okotemmanuel <okotemmanuel22@gmail.com>
2018-07-10 20:31:45 +0300

    pay

--- website/payleo.php
+++ website/payleo.php
@@ -1,23 +1,20 @@
 
 <?php
 
-$merchantCode  = '54540';
-$amount = 1000;
-$callbackUrl = 'elijah-mcquinn.com'; 
-$consumerSecret= '3z9J2HBzk6TQI1LHBqv7oIZlc31529671271';
-
-$data = 'https://webpay.pay-leo.com&'.$merchantCode.'&'.$amount.'&'.$callbackUrl ;
-
-$signature = hash_hmac ('sha256', $data,$consumerSecret);
+$merchantCode  = "54540";
+$amount = "1000";
+$callbackUrl = "elijah-mcquinn.com"; 
+$consumerSecret= "3z9J2HBzk6TQI1LHBqv7oIZlc31529671271" ;
 
 
 
-echo '<a href="https://webpay.pay-leo.com?amount='.$amount.'&merchantCode='.$merchantCode.'&auth_signature='.$signature.'&callbackUrl='.$callbackUrl.'" >click here to pay </a>';
+$data ="https://webpay.payleo.com&".$merchantCode."&".$amount."&".$callbackUrl;
+ $signature = hash_hmac ("sha256", $data, $consumerSecret);
+
+echo '<a href="https://webpay.pay-leo.com?amount='.$amount.'&merchantCode='.$merchantCode.'&auth_signature='.$signature.'&callbackUrl='.$callbackUrl.'" >click here to pay </a><br>';
+echo 'https://webpay.pay-leo.com?amount='.$amount.'&merchantCode='.$merchantCode.'&auth_signature='.$signature.'&callbackUrl='.$callbackUrl;
 
 
-//$_GET['code'];
-//$_GET['message'];
-//$_GET['transactionId'];
 
 
 
